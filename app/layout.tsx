184756966466565a8370073d65816a09089605d8
GAUTAM MANAK <106014185+gautammanak1@users.noreply.github.com>
2026-05-13 10:07:55 +0530

    adds footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Cormorant_Garamond  ,Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "./components/sections/footer";
 
 const cormorant = Cormorant_Garamond({
   subsets: ["latin"],
@@ -29,7 +30,7 @@ export default function RootLayout({
       className={`  ${cormorant.variable}
         ${inter.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">{children}<Footer /></body>
     </html>
   );
 }
